0487171cd97d63157f4b8fcff34f660ce8fed336
sandro132 <mianveo@hotmail.com>
2023-08-30 15:49:59 -0500

    add backend of the forget password and recuperation of them

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,5 +1,6 @@
 import Usuario from"../models/Usuario.js"
 import generarId from "../helpers/generarId.js";
+import generarJWT from "../helpers/generarJWT.js";
 
 const registrar = async (req, res) => {
     // Evitar registros duplicados
@@ -47,11 +48,62 @@ const autenticar = async (req, res) => {
             _id: usuario._id,
             nombre: usuario.nombre,
             email: usuario.email,
-        })
+            token: generarJWT(usuario._id),
+        });
     } else {
         const error = new Error("El password es incorrecto");
         return res.status(403).json({ msg: error.message });
     }
 };
 
-export { registrar, autenticar };
+// token hasheado
+const confirmar = async (req, res) => {
+    const { token } = req.params;
+    const usuarioConfirmar = await Usuario.findOne({ token });
+    if(!usuarioConfirmar) {
+        const error = new Error("Token no valido");
+        return res.status(403).json({ msg: error.message });
+    };
+
+    try {
+        usuarioConfirmar.confirmado = true
+        usuarioConfirmar.token = "";
+        await usuarioConfirmar.save();
+        res.json({ msg: "Usuario confirmado correctamente "})
+        
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+    const olvidePassword = async (req, res) => {
+        const { email } = req.body;
+        const usuario = await Usuario.findOne({email})
+        if(!usuario) {
+            const error = new Error("El usuario no existe");
+            return res.status(404).json({ msg: error.message });
+        };
+
+        try {
+            usuario.token = generarId();
+            await usuario.save();
+            res.json({ msg: "Hemos enviado un email con las instrucciones" });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const comprobarToken = async (req, res) => {
+        const { token } = req.params;
+
+        const tokenValido = await Usuario.findOne({ token });
+
+        if(tokenValido) {
+            res.json({ msg: "Token valido y el usuario existe" });
+        } else {
+            const error = new Error("Token no valido");
+            return res.status(404).json({ msg: error.message });
+        }
+    };
+
+export { registrar, autenticar, confirmar, olvidePassword, comprobarToken };
--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,10 +1,19 @@
 import express from "express";
 const router = express.Router();
-import { registrar, autenticar } from "../controllers/usuarioController.js"
+import { 
+    registrar,
+    autenticar,
+    confirmar,
+    olvidePassword,
+    comprobarToken
+} from "../controllers/usuarioController.js"
 
 // Autenticacion, Registro y confirmacion de Usuarios
 
 router.post("/", registrar)
 router.post("/login", autenticar)
+router.get("/confirmar/:token", confirmar)
+router.post("/olvide-password", olvidePassword);
+router.get("/olvide-password/:token", comprobarToken);
 
 export default router;
